7e8d351eb78cc792fc8780f93e1c44ed920f07e6
Uky <ukyaaery+code@gmail.com>
2009-10-09 22:04:26 -0700

    reworked script to not require clicking

--- yt-linkify_pl_text.user.js
+++ yt-linkify_pl_text.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name          YouTube Channel Playlist Linkifier
 // @namespace     http://github.com/uky/greasemonkey
-// @description   Changes the currently visible plain text playlist name to link to the old classic playlist page when you click on the page.
-// @version       0.1
+// @description   Changes plain text playlist names to link to their classic playlist pages.
+// @version       0.2.0
 // @author        Uky
 // @include       http://www.youtube.com/*
 // @exclude       http://www.youtube.com/watch?*
 // ==/UserScript==
 
-// A nice wrapper for XPATH queries. Defaults to document root node.
+// A nice wrapper for XPath queries. Defaults to document root node.
 function xpath(query, node) {
 	return document.evaluate(query,
 			typeof(node) !== 'undefined' ? node : document,
@@ -17,14 +17,25 @@ function xpath(query, node) {
 			null);
 }
 
-// Check if we're on a user channel.
+// Check if we're on a new-style user channel.
 var channel_check = xpath("//style[@id='channel-theme-css']");
-if (channel_check.snapshotLength == 0)
-	GM_log('not on user channel. exiting.');
+if (channel_check.snapshotLength == 0) {
 	return;
-GM_log('on a user channel. running...');
+}
+
+function linkify(event) {
+	//GM_log(event.target.id);
+
+	if (!event.target.id) {
+		return;
+	}
+
+	// Only check relevant DOM updates otherwise the script can
+	// become very slow.
+	if (!event.target.id.match('playnav-play-playlist-')) {
+		return;
+	}
 	
-function linkify() {
 	var title_nodes = xpath("//div[@class='playnav-playlist-header']/div[@class='title']");
 	for (var i = 0; i < title_nodes.snapshotLength; i++) {
 		var node = title_nodes.snapshotItem(i);
@@ -35,10 +46,5 @@ function linkify() {
 	}
 }
 
-document.addEventListener('click', linkify, true );
-
-// Ideally the script would automatically linkify as the page is
-// altered but I'm not sure of a good way to do that...
-// This simple way runs very slowly:
-//document.addEventListener('DOMNodeInserted', linkify, true );
-//document.addEventListener('DOMAttrModified', linkify, true );
+document.addEventListener('DOMNodeInserted', linkify, true );
+document.addEventListener('DOMAttrModified', linkify, true );

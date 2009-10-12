1d59918d3aa23077f30bd5225eeef4c065cd16b2
Uky <ukyaaery+code@gmail.com>
2009-10-11 19:11:04 -0700

    better compatibility with Webmonkey

--- yt-linkify_pl_text.user.js
+++ yt-linkify_pl_text.user.js
@@ -2,7 +2,7 @@
 // @name          YouTube Channel Playlist Linkifier
 // @namespace     http://github.com/uky/greasemonkey
 // @description   Changes plain text playlist names to link to their classic playlist pages.
-// @version       0.2.1
+// @version       0.2.2
 // @author        Uky
 // @include       http://www.youtube.com/*
 // @exclude       http://www.youtube.com/watch?*
@@ -18,14 +18,8 @@ function xpath(query, node) {
 			null);
 }
 
-// Check if we're on a new-style user channel.
-var channel_check = xpath("//style[@id='channel-theme-css']");
-if (channel_check.snapshotLength == 0) {
-	return;
-}
-
 function linkify(event) {
-	GM_log(event.target.id);
+	//GM_log(event.target.id);
 
 	if (!event.target.id) {
 		return;
@@ -42,10 +36,14 @@ function linkify(event) {
 		var node = title_nodes.snapshotItem(i);
 		var playlist_id = node.getAttribute('id').match('playnav-playlist-([0-9A-F]+)-title')[1];
 		var playlist_name = xpath(".//text()", node).snapshotItem(0).nodeValue;
-		GM_log('linkifying playlist ' + playlist_name + '(' + playlist_id + ')');
+		//GM_log('linkifying playlist ' + playlist_name + '(' + playlist_id + ')');
 		node.innerHTML = "<a href='http://www.youtube.com/view_play_list?p=" + playlist_id + "'>" + playlist_name + "</a>";
 	}
 }
 
-document.addEventListener('DOMNodeInserted', linkify, true );
-document.addEventListener('DOMAttrModified', linkify, true );
+// Check if we're on a new-style user channel.
+var channel_check = xpath("//style[@id='channel-theme-css']");
+if (channel_check.snapshotLength > 0) {
+	document.addEventListener('DOMNodeInserted', linkify, true );
+	document.addEventListener('DOMAttrModified', linkify, true );
+}

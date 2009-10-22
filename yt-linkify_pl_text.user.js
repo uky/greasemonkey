9e2de566ecc6f327067abb1921f267f08df39159
Uky <ukyaaery+code@gmail.com>
2009-10-21 18:15:16 -0700

    Channel detection now based on //div[@id='channel-body'] instead of //style[@id='channel-theme-css']. Less likely other scripts will remove the channel-body node and there doesn't seem to be any old-style channels left to worry about differentiation between the two.

--- yt-linkify_pl_text.user.js
+++ yt-linkify_pl_text.user.js
@@ -2,7 +2,7 @@
 // @name          YouTube Channel Playlist Linkifier
 // @namespace     http://github.com/uky/greasemonkey
 // @description   Changes plain text playlist names to link to their classic playlist pages.
-// @version       0.2.2
+// @version       0.2.3
 // @author        Uky
 // @include       http://www.youtube.com/*
 // @exclude       http://www.youtube.com/watch?*
@@ -19,8 +19,6 @@ function xpath(query, node) {
 }
 
 function linkify(event) {
-	//GM_log(event.target.id);
-
 	if (!event.target.id) {
 		return;
 	}
@@ -36,13 +34,13 @@ function linkify(event) {
 		var node = title_nodes.snapshotItem(i);
 		var playlist_id = node.getAttribute('id').match('playnav-playlist-([0-9A-F]+)-title')[1];
 		var playlist_name = xpath(".//text()", node).snapshotItem(0).nodeValue;
-		//GM_log('linkifying playlist ' + playlist_name + '(' + playlist_id + ')');
 		node.innerHTML = "<a href='http://www.youtube.com/view_play_list?p=" + playlist_id + "'>" + playlist_name + "</a>";
 	}
 }
 
 // Check if we're on a new-style user channel.
-var channel_check = xpath("//style[@id='channel-theme-css']");
+//var channel_check = xpath("//style[@id='channel-theme-css']"); // There's a script on USo (59894) that removes this node...
+var channel_check = xpath("//div[@id='channel-body']"); // This node should (hopefully) never be removed.
 if (channel_check.snapshotLength > 0) {
 	document.addEventListener('DOMNodeInserted', linkify, true );
 	document.addEventListener('DOMAttrModified', linkify, true );

5825cfb8d6282545d3b99cfdabd1a1a1d9dab425
uky <ukyaaery@gmail.com>
2012-12-09 08:27:51 -0800

    Update to YouTube's latest layout changes.

--- yt-fit_video_to_window.user.js
+++ yt-fit_video_to_window.user.js
@@ -1,55 +1,70 @@
 // ==UserScript==
 // @name           YouTube Fit Video to Window
 // @namespace      http://github.com/uky/greasemonkey
-// @downloadURL    https://github.com/uky/greasemonkey/raw/release/yt-fit_video_to_window.user.js
-// @description    Enlarges video to the browser's window size.
-// @version        0.1.2
 // @author         Uky
-// @match          http*://www.youtube.com/watch?*
-// @grant          GM_addStyle
+// @description    Enlarges video to the browser's window size.
+// @version        0.1.3
+// @updateURL      https://github.com/uky/greasemonkey/raw/release/yt-fit_video_to_window.meta.js
+// @downloadURL    https://github.com/uky/greasemonkey/raw/release/yt-fit_video_to_window.user.js
+// @grant          none
+// @include        http://www.youtube.com/watch?*
+// @include        https://www.youtube.com/watch?*
 // ==/UserScript==
 
+
 // [ Constants ]
-var header_ID = 'watch-headline-user-info';
-var player_ID = 'watch-player';
-var video_ID = 'watch-video';
-var extra_ID = 'watch-video-extra';
+var header_ID = 'watch7-headline';
+var player_ID = 'watch7-player';
+var video_ID = 'watch7-video';
+
 
 // [ Derived Values ]
-var header_height = $(header_ID).offsetHeight;
+var header_height = 0;
+var header = document.getElementById(header_ID);
+if (header != null)
+	header_height = header.offsetHeight;
 
-// [ Helper Functions ]
-function $(A) { return document.getElementById(A); }
 
 // [ Main Script ]
 
+// Remove guide for extra horizontal viewing area.
+var body = document.getElementsByTagName('body')[0];
+if (body != null) {
+	if (body.classList.contains('guide-enabled'))
+		body.classList.remove('guide-enabled');
+	if (body.classList.contains('guide-expanded')) {
+		body.classList.remove('guide-expanded');
+		body.classList.add('guide-collapsed');
+	}
+}
+var video_container = document.getElementById('watch7-video-container');
+if (video_container != null)
+	video_container.style.paddingLeft = 0;
+
+
 // Force wide mode so the sidebar doesn't get in the way of the video.
-var A = $('page');
-if (A.className.indexOf('watch-wide') == -1)
-	A.className += ' watch-wide';
+var container = document.getElementById('watch7-container');
+var wide_class = 'watch-wide';
+if (container != null && !container.classList.contains(wide_class))
+	container.classList.add(wide_class);
 
-var B = $(video_ID);
-if (B.className.indexOf('medium') == -1)
-	B.className += ' medium';
-
-GM_addStyle('#watch-sidebar { margin-top: 5px !important; }');
-
-// Prevent centering with auto margins.
-GM_addStyle('#watch-video, #watch-player { margin: 0 !important; }');
-
-// Remove extra info at bottom of video for more room
-GM_addStyle('#' + extra_ID + ' { display: none; }');
 
 // Resize the video to fit the browser window.
 function fitToWindow() {
-	var player=$(player_ID);
-	var video=$(video_ID);
-	video.style.width = document.body.offsetWidth + 'px';
-	player.style.width = document.body.offsetWidth + 'px';
-	player.style.height = (document.body.offsetHeight - header_height - 5) + 'px';
+	var player = document.getElementById(player_ID);
+	var video = document.getElementById(video_ID);
+	if (video != null)
+		video.style.width = document.body.offsetWidth + 'px';
+	if (player != null) {
+		player.style.width = document.body.offsetWidth + 'px';
+		player.style.height = (document.body.offsetHeight - header_height - 5) + 'px';
+	}
 }
 window.addEventListener('resize', fitToWindow, false);
 fitToWindow();
 
+
 // Scroll past the masthead.
-$(player_ID).scrollIntoView(true);
+var player = document.getElementById(player_ID);
+if (player != null)
+	player.scrollIntoView(true);

d4d5d4dcf12b8b94ddb51c7c0b17eaf1947e6c7b
uky <ukyaaery@gmail.com>
2012-12-09 08:28:35 -0800

    Add metadata file for updating.

---
+++ yt-fit_video_to_window.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           YouTube Fit Video to Window
+// @namespace      http://github.com/uky/greasemonkey
+// @author         Uky
+// @description    Enlarges video to the browser's window size.
+// @version        0.1.3
+// @downloadURL    https://github.com/uky/greasemonkey/raw/release/yt-fit_video_to_window.user.js
+// @grant          none
+// @include        http://www.youtube.com/watch?*
+// @include        https://www.youtube.com/watch?*
+// ==/UserScript==

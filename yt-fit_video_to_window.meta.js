058f29aae8084c8d03dbe36ae5170929a52f27cf
uky <ukyaaery@gmail.com>
2013-03-20 18:22:11 -0700

    Adjust to watch page changes.

--- yt-fit_video_to_window.meta.js
+++ yt-fit_video_to_window.meta.js
@@ -3,7 +3,7 @@
 // @namespace      http://github.com/uky/greasemonkey
 // @author         Uky
 // @description    Resizes video to the browser's window size.
-// @version        2012.12.13
+// @version        2013.03.20
 // @updateURL      https://github.com/uky/greasemonkey/raw/release/yt-fit_video_to_window.meta.js
 // @downloadURL    https://github.com/uky/greasemonkey/raw/release/yt-fit_video_to_window.user.js
 // @grant          none
